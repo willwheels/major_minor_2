578c478d1fde534f34876b1d80955a397ea900ca
willwheels <wheeler.william@epa.gov>
2024-04-25 15:00:21 -0400

    set up directory

---
+++ 00_setup.R
@@ -0,0 +1,11 @@
+
+dir_create_if_not_exist <- function(dir_name, ...) {
+  
+  
+  if (!dir.exists(here::here(dir_name, ...))) {dir.create(here::here(dir_name, ...))}
+}
+
+dir_create_if_not_exist("data")
+dir_create_if_not_exist("data", "csv_files")
+dir_create_if_not_exist("data", "R_data_files")
+
