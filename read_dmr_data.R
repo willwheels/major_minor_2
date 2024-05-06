fca1b3e84600064ba739c00969a59000f195d9c5
willwheels <wheeler.william@epa.gov>
2024-05-06 17:12:17 -0400

    add file to read DMRs

---
+++ read_dmr_data.R
@@ -0,0 +1,82 @@
+library(tidytable)
+
+#remotes::install_version("data.table", version = "1.15.0")
+
+options(timeout = 300)
+
+download_dmr_file <- function(dmr_year) {
+  
+  
+  path = here::here("data", "zip_files")
+  
+  
+  dmr_url <- paste0("https://echo.epa.gov/files/echodownloads/npdes_dmrs_fy",
+                    dmr_year,
+                    ".zip")
+  
+  
+  download_filename_full <- stringr::str_split_1(dmr_url, "/")
+  download_filename_full <- download_filename_full[length(download_filename_full)]
+  #filename_no_ext <- stringr::str_split_1(filename_full, "\\.")[1]
+  
+  destination_file <- here::here(path, download_filename_full)
+  
+  
+  if(!file.exists(destination_file)) {
+    print(paste(dmr_year, "downloading"))
+    download.file(dmr_url, destfile = destination_file, mode = "wb")}
+  else {print(paste(dmr_year, " already downloaded"))}
+  
+}
+
+
+fread_dmrs <- function(dmr_year) {
+  
+  print(paste("process dmr files for",  dmr_year))
+  
+  dmr_file <- here::here("data", "zip_files", paste0("npdes_dmrs_fy", dmr_year, ".zip"))
+  
+  dmr_data <- fread(dmr_file, 
+                    select = c(EXTERNAL_PERMIT_NMBR = "character",
+                               VERSION_NMBR = "integer",
+                               PERM_FEATURE_NMBR = "character",
+                               PERM_FEATURE_TYPE_CODE = "character",
+                               MONITORING_LOCATION_CODE = "character",
+                               PARAMETER_CODE = "character",
+                               PARAMETER_DESC = "character", 
+                               LIMIT_TYPE_CODE = "character",
+                               LIMIT_VALUE_NMBR = "numeric",
+                               LIMIT_UNIT_CODE  = "character",
+                               LIMIT_VALUE_QUALIFIER_CODE = "character",
+                               DMR_VALUE_NMBR = "numeric",
+                               DMR_UNIT_CODE = "character",
+                               DMR_VALUE_QUALIFIER_CODE = "character",
+                               EXCEEDENCE_PCT = "integer64",
+                               STATISTICAL_BASE_CODE = "character",
+                               MONITORING_PERIOD_END_DATE = "character",
+                               VALUE_RECEIVED_DATE = "character", 
+                               DAYS_LATE = "integer",
+                               VIOLATION_CODE = "character"
+        )
+  )
+  
+  dmr_data <- dmr_data %>%
+    mutate(monitoring_period_end_date2 = lubridate::mdy(MONITORING_PERIOD_END_DATE),
+           value_received_date2 = lubridate::mdy(VALUE_RECEIVED_DATE)) %>%
+    select(-MONITORING_PERIOD_END_DATE, -VALUE_RECEIVED_DATE)
+  
+  
+
+  
+  save(dmr_data, file = here::here("data", "R_data_files", paste0("dmr_data_", dmr_year, ".Rda")))
+  
+}
+
+
+
+
+dmr_years <- as.character(2018:2023)
+
+purrr::walk(dmr_years, download_dmr_file)
+
+purrr::walk(dmr_years, fread_dmrs)
