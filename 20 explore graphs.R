d01d98631106dc70c7df986f508e9a7bfffde234
tandarge <tihitina.t.andarge@gmail.com>
2024-05-31 13:03:21 -0400

    error bars

--- 20 explore graphs.R
+++ 20 explore graphs.R
@@ -7,14 +7,14 @@ load(here::here("data", "R_data_files", "all_potw_permits_most_recent.Rda"))
 
 source(here::here("theme_tina.R"))
 
-ggplot(icis_permits2 %>% filter(design_flow_round <= 5, design_flow_round > 0), 
-       aes(x = design_flow_round)) +
-  geom_histogram(binwidth = .1) + 
+ggplot(icis_permits2 %>% filter(design_flow_round_one_decimal <= 5, design_flow_round_one_decimal > 0), 
+       aes(x = design_flow_round_one_decimal)) +
+  geom_histogram(binwidth = .1, color = "white") + 
   annotate("rect", xmin = .7, xmax = 1.2, ymin = 250, ymax = 450, alpha = .2, color = "red") +
-  annotate("text", x = 1.5, y = 1500, label = "1 MGD") +
+  annotate("text", x = 1.5, y = 6700, label = "1 MGD") +
   geom_vline(xintercept = 1, linetype = 2) +
   #labs(title = "Histogram of POTW Flows") +
-  xlab("Design Flow (rounded to single decimal)") + ylab("Count") +
+  xlab("Design Flow (MGD, 0.1 width bins)") + ylab("Count") +
   theme_tina
 
 ggsave("design_flow_hist.png", path = here::here("figs"),
@@ -26,10 +26,27 @@ icis_permits2 %>%
   group_by(EXTERNAL_PERMIT_NMBR) %>%             ## keep only most recent permit
   slice(1) %>%                               ##
   ungroup() %>%
-  filter(design_flow_round <= 5, design_flow_round > 0) %>%
-  ggplot(aes(x = design_flow_round)) +
+  filter(design_flow_round_one_decimal <= 5, design_flow_round_one_decimal > 0) %>%
+  ggplot(aes(x = design_flow_round_one_decimal)) +
   geom_histogram(binwidth = .1) +
   theme_minimal()
+
+ggplot(icis_permits2 %>% arrange(EXTERNAL_PERMIT_NMBR, VERSION_NMBR) %>%
+         group_by(EXTERNAL_PERMIT_NMBR) %>%             ## keep only most recent permit
+         slice(1) %>%                               ##
+         ungroup() %>%
+         filter(design_flow_round_one_decimal <= 5, design_flow_round_one_decimal >= 0.1),
+       aes(x = design_flow_round_one_decimal)) +
+  geom_histogram(binwidth = .1, color = "white") + 
+  annotate("rect", xmin = .7, xmax = 1.2, ymin = 250, ymax = 450, alpha = .2, color = "red") +
+  annotate("text", x = 1.5, y = 2000, label = "1 MGD") +
+  geom_vline(xintercept = 1, linetype = 2) +
+  #labs(title = "Histogram of POTW Flows") +
+  xlab("Design Flow (MGD, 0.1 width bins)") + ylab("Count") +
+  theme_tina
+
+ggsave("design_flow_hist.png", path = here::here("figs"),
+       h = 8.5, w = 11, units = "in", bg = "white")
 
 
 ggplot(icis_permits2 %>% filter(design_flow_round_smaller >= .8, design_flow_round_small <= 1.2), 
@@ -79,25 +96,51 @@ ggplot(icis_permits2 %>% filter(flow_over_design < 5, flow_over_design > -5, TOT
   geom_point() + 
   theme_minimal()
 
+icis_permits_most_recent_summarized <- icis_permits2 %>%
+  group_by(EXTERNAL_PERMIT_NMBR) %>%             ## keep only most recent permit
+  slice(1) %>%                                   ##
+  ungroup() %>%
+  mutate(e90_ratio = num_e90/num_limits_per_year) %>%
+  filter(TOTAL_DESIGN_FLOW_NMBR <= 10) %>%
+  group_by(design_flow_round_one_decimal) %>%
+  summarise(mean_snc = mean(num_snc, na.rm = TRUE), 
+            sd_snc = sd(num_snc, na.rm = TRUE),
+            mean_e90 = mean(num_e90, na.rm = TRUE),
+            mean_num_limits_per_year = mean(num_limits_per_year, na.rm = TRUE),
+            mean_inspections = mean(num_inspections, na.rm = TRUE),
+            sd_inspections = sd(num_inspections, na.rm = TRUE),
+            mean_CEI = mean(num_CEI, na.rm = TRUE),
+            mean_e90_ratio = mean(e90_ratio, na.rm = TRUE),
+            num_in_bin = n()) %>%
+  arrange(desc(design_flow_round_one_decimal)) %>%
+  ungroup()
 
 
 
 icis_permits3 <- icis_permits_most_recent_summarized %>%
-  filter(design_flow_round <= 2)
+  filter(design_flow_round_one_decimal <= 2)
 
 
 
-ggplot(icis_permits3, aes(x = design_flow_round, y = mean_snc)) +
+ggplot(icis_permits3, aes(x = design_flow_round_one_decimal, y = mean_snc)) +
   geom_line() +
   labs(title = "Mean Number of Quarters in SNC Since 2018 by Design Flow") +
   ylab("Mean Quarters in SNC") + xlab("Design Flow (rounded to single decimal)") +
   theme_minimal()
 
 
-ggplot(icis_permits3, aes(x = design_flow_round, y = mean_snc, size = num_in_bin)) +
-  geom_point() +
+ggplot(icis_permits3, aes(x = design_flow_round_one_decimal, y = mean_snc, size = num_in_bin)) +
+  geom_point(color = "#0082CB", alpha = 0.8) +  
+  geom_errorbar(aes(ymin = mean_snc - 1.96*sd_snc/sqrt(num_in_bin), 
+                    ymax = mean_snc + 1.96*sd_snc/sqrt(num_in_bin)),
+                width = 0.03, size = 0.4, show.legend = FALSE) +
+  annotate("text", x = 1.05, 
+           y = max(subset(icis_permits3, design_flow_round_one_decimal <=2)$mean_snc)*1.1, 
+           label = "1 MGD", size = 10) +
+  geom_vline(xintercept = 1, linetype = 2) +
+  labs(size = "No. Permits in Bin" ) +
   #labs(title = "Mean Number of Quarters in SNC Since 2018 by Design Flow") +
-  ylab("Mean Quarters in SNC") + xlab("Design Flow (rounded to single decimal)") +
+  ylab("Mean Quarters in SNC") + xlab("Design Flow") +
   theme_tina
 
 ggsave("snc_by_design_flow_dots.png", path = here::here("figs"),
@@ -127,12 +170,17 @@ ggplot(icis_permits3, aes(x = design_flow_round, y = mean_inspections)) +
   theme_minimal()
 
 
-ggplot(icis_permits3, aes(x = design_flow_round, y = mean_inspections, size = num_in_bin)) +
-  geom_point() +
-  annotate("text", x = 1.2, y = 5.75, label = "1 MGD") +
+ggplot(icis_permits3, aes(x = design_flow_round_one_decimal, y = mean_inspections, size = num_in_bin)) +
+  geom_point(color = "#0082CB", alpha = 0.8) +  
+  geom_errorbar(aes(ymin = mean_inspections - 1.96*sd_inspections/sqrt(num_in_bin), 
+                    ymax = mean_inspections + 1.96*sd_inspections/sqrt(num_in_bin)),
+                width = 0.03, size = 0.4, show.legend = FALSE) +
+  annotate("text", x = 1.05, 
+           y = max(subset(icis_permits3, design_flow_round_one_decimal <=2)$mean_inspections) + 2, 
+           label = "1 MGD", size = 10) +
   geom_vline(xintercept = 1, linetype = 2) +
-  labs(title = "Mean Number of Inspections Since 2018 by Design Flow") +
-  ylab("Mean Number Inspectionss") + xlab("Design Flow (rounded to single decimal)") +
+  labs(size = "No. Permits in Bin" ) +
+  ylab("Mean Number of Inspections") + xlab("Design Flow") +
   theme_tina
 
 ggsave("inspections_by_design_flow_dots.png",  path = here::here("figs"),
